94e2baa1df84b14c5ccd5fe92d8c5a8b31fea066
HassanSamine <saminehassan@gmail.com>
2024-06-09 17:33:28 +0200

    Create canvas.js

---
+++ canvas.js
@@ -0,0 +1,14 @@
+// Load the HTTP module
+const http = require('http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!\n');
+});
+
+// Listen on port 3000
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Server running at http://127.0.0.1:3000/');
+});
