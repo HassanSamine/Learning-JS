b0427119a66ac230c33641f101702c4322d3af1e
HassanSamine <saminehassan@gmail.com>
2024-07-03 16:46:12 +0200

    Adding functions

--- Project 3 - Quizz app/script.js
+++ Project 3 - Quizz app/script.js
@@ -60,16 +60,58 @@ function showQuestion() {
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+
     });
 
 }
 
-// function resetState() {
-//     nextButton.style.display = "none";
-//     while (answerButtons.firstChild) {
-//         answerButtons.removeChild(answerButtons.firstChild);
-//     }
-// }
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+
+    }else {
+        showScore();
+    }
+}
+function showScore(){
+    answerButtons.innerHTML = '';
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+
+})
 
 startQuiz();
 
